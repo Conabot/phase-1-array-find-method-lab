1a74fe185e0214be0db03b93c9e1c5190423f22b
Minh Tran <tdaimike@gmail.com>
2023-04-01 01:04:32 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+/*const superbowlWin = record.findLast(({result}) => {
+    if (result === 'W'){
+        return superbowlWin;
+    } else {
+        return undefined;
+    }
+})*/
+
+function superbowlWin(record) {
+    const denverWinObj = record.find(({result}) => result === "W" );
+    if (denverWinObj) {
+      return denverWinObj.year;
+    } 
+    return undefined;
+  
+}
+
